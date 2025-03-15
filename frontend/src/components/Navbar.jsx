c123e01c5b0467bf94a6ada98744516ef89c1525
adityalaxkar123 <adityalaxkar9876@gmail.com>
2025-03-15 16:51:51 +0530

    added chatbot frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -104,9 +104,9 @@ const Navbar = () => {
           <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
             <NavLink  onClick={()=> setShowmenu(false)} to='/'><p className='px-4 py-2 rounded inline-block'>Home</p></NavLink>
             <NavLink  onClick={()=> setShowmenu(false)} to='/doctors'><p className='px-4 py-2 rounded inline-block'>All Doctors</p></NavLink>
+            <NavLink  onClick={()=> setShowmenu(false)} to='/store'><p className='px-4 py-2 rounded inline-block'>Store</p></NavLink>
             <NavLink  onClick={()=> setShowmenu(false)} to='/about'><p className='px-4 py-2 rounded inline-block'>About</p></NavLink>
             <NavLink  onClick={()=> setShowmenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block'>Contact</p></NavLink>
-            <NavLink  onClick={()=> setShowmenu(false)} to='/store'><p className='px-4 py-2 rounded inline-block'>Store</p></NavLink>
           </ul>
         </div>
       </div>
